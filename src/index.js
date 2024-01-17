e689da385301d329dd6554babcaab84fb26d44f0
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-01-17 13:38:43 +0000

    Added search functionnality updating temperature and city name using APIs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-function handleSearchFormSubmit(event) {
+
+function displayCity(city) {
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = city;
+}
+
+function displayTemperatureValue(temperature) {
+    let TempElement = document.querySelector(".current-temperature-value");
+    TempElement.innerHTML = Math.round(temperature);
+}
+function refreshWeather(response) {
+    console.log(response);
+    let data = response.data;
+    if (data.city !== undefined) {
+        displayCity(data.city);
+        displayTemperatureValue(data.temperature.current);
+    }
+}
+ // Update the API URL with the newCityname
+ function cityUpdate(city){
+ let apiKey = "ec0ft3ef184fa26o40bf0860bad82dc8";
+ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+// Make the API call
+axios.get(apiUrl).then(refreshWeather);}
+ 
+ function showNewCityInfo(event) {
     event.preventDefault();
-    let userInput = document.querySelector("#search-input");
-    let cityDisplay = document.querySelector("#current-city");
-    cityDisplay.innerHTML = userInput.value;
+    let newCityname = "Paris"; // Assuming a default value
+    let inputCity = document.querySelector("#search-input");
+    newCityname = inputCity.value.trim();
+    cityUpdate(newCityname);
   }
   
-  let searchForm = document.querySelector("#city-form");
-  searchForm.addEventListener("submit", handleSearchFormSubmit);
-  
+  let form = document.querySelector("#city-form");
+  form.addEventListener("submit", showNewCityInfo);
