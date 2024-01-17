2fb23ce370c14e3c7620362f4de344da12637565
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-01-17 13:44:47 +0000

    Added JavaScript to display time, humidity, wind speed, and weather condition in the project.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 
-function displayCity(city) {
+function formatDate(date) {
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let currentDay = days[date.getDay()];
+    let currentHour = date.getHours();
+    let currentMinutes = date.getMinutes();
+    if (currentMinutes < 10) {
+      currentMinutes = `0${currentMinutes}`;
+    }
+  
+    if (currentHour < 10) {
+      currentHour = `0${currentHour}`;
+    }
+    let nowDate = `${currentDay} ${currentHour}:${currentMinutes}`;
+    return nowDate;
+  }
+  
+  function displayTime(timeUpdate){
+  let time = document.querySelector("#date-now");
+  time.innerHTML = formatDate(new Date (timeUpdate));
+}
+  
+  function displayCity(city) {
     let cityElement = document.querySelector("#current-city");
     cityElement.innerHTML = city;
 }
@@ -8,14 +37,35 @@ function displayTemperatureValue(temperature) {
     let TempElement = document.querySelector(".current-temperature-value");
     TempElement.innerHTML = Math.round(temperature);
 }
+
+function displayHumidity(humidity) {
+    let humidityElement = document.querySelector("#current-humidity");
+    humidityElement.innerHTML = `${humidity}%`;
+}
+
+function displayWindSpeed(windSpeed) {
+    let windSpeedElement = document.querySelector("#current-wind");
+    windSpeedElement.innerHTML = `${windSpeed} Km/h`;
+}
+
+function displayDescription(description) {
+    let descriptionElement = document.querySelector("#current-description");
+    descriptionElement.innerHTML = `${description}`;
+}
+
 function refreshWeather(response) {
     console.log(response);
     let data = response.data;
     if (data.city !== undefined) {
+        displayTime(data.time*1000);
         displayCity(data.city);
         displayTemperatureValue(data.temperature.current);
+        displayHumidity(data.temperature.humidity);
+        displayWindSpeed(data.wind.speed);
+        displayDescription(data.condition.description);
     }
 }
+
  // Update the API URL with the newCityname
  function cityUpdate(city){
  let apiKey = "ec0ft3ef184fa26o40bf0860bad82dc8";
@@ -33,3 +83,5 @@ axios.get(apiUrl).then(refreshWeather);}
   
   let form = document.querySelector("#city-form");
   form.addEventListener("submit", showNewCityInfo);
+  
+ 
